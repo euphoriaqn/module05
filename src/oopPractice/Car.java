f648f26c2fbc9d0cbe97cdd6c93202b380ede708
euphoriaqn <euphoriaqn@gmail.com>
2017-07-26 17:34:28 +0300

    Додано функціонал

--- src/oopPractice/Car.java
+++ src/oopPractice/Car.java
@@ -4,22 +4,21 @@ package oopPractice;
  * Created by user22 on 24.07.2017.
  */
 public class Car {
-    private int count = 0;
     CarWheel[] otherCarwheel;
     CarDoor[] otherCarDoor;
     private int[] dateOfManufacture = new int[3];
     private String typeOfEngine;
     private int maxSpeedOfCar;
     private float accelerationTime;
-    private byte numberOfPassanger;
-    private byte numberOfPassangerNow;
+    private int numberOfPassanger;
+    private int numberOfPassangerNow;
     private int currentSpeed;
     public Car(int date, int month, int year) {
         this.dateOfManufacture[0] = date;
         this.dateOfManufacture[1] = month;
         this.dateOfManufacture[2] = year;
     }
-    public Car(int date, int month, int year, String typeOfEngine, int maxSpeedOfCar, float accelerationTime, byte numberOfPassanger, byte numberOfPassangerNow, int currentSpeed){
+    public Car(int date, int month, int year, String typeOfEngine, int maxSpeedOfCar, float accelerationTime, int numberOfPassanger, int numberOfPassangerNow, int currentSpeed){
         this.dateOfManufacture[0] = date;
         this.dateOfManufacture[1] = month;
         this.dateOfManufacture[2] = year;
@@ -54,6 +53,7 @@ public class Car {
         }
     }
     public int numberOfWheels(){
+        int count = 0;
         for (int i =0; i<otherCarwheel.length; i++)
         {
             if (otherCarwheel[i].getTireState() > 0){
@@ -62,25 +62,48 @@ public class Car {
         }
         return count;
     }
- /*  public CarWheel[] addNewWheels(int index){
+    public int numberOfDestroyedWheels(){
+        int countDestroyedWheel = 0;
+        for (int i =0; i<otherCarwheel.length; i++)
+        {
+            if (otherCarwheel[i].getTireState() == 0) countDestroyedWheel++;
+        }
+        return countDestroyedWheel;
+    }
+    public void addNewWheels(int index){
 
-    otherCarwheel.length + index
+    CarWheel[] newOtherCarwheel= new CarWheel[this.otherCarwheel.length+index];
+        for(int i = 0; i < otherCarwheel.length; i++)
+            newOtherCarwheel[i] = otherCarwheel[i];
+    for (int i =otherCarwheel.length; i <newOtherCarwheel.length; i ++)
+    {newOtherCarwheel[i] = new CarWheel(1);}
+    this.otherCarwheel = newOtherCarwheel;
     }
- /*   public int currentMaxSpeed (){
-        int maxErasedWheel = otherCarwheel[]
-        for
+    public int currentMaxSpeed () {
+        int currentmaxSpeed;
+        double mostErasedWheel = otherCarwheel[0].getTireState();
+        if (numberOfPassangerNow == 0) currentmaxSpeed = 0;
+        else {
+            for (int i = 0; i < otherCarwheel.length; i++) {
+                if (otherCarwheel[i].getTireState() < mostErasedWheel) {
+                    mostErasedWheel = otherCarwheel[i].getTireState();
+                }
+
+            }
+            currentmaxSpeed = (int)(maxSpeedOfCar*mostErasedWheel);
+                   }
+        return currentmaxSpeed;
     }
-    */
-     public void showInfoCar(){
+    public void showInfoCar(){
          System.out.println("Дата виробництва " + dateOfManufacture[0] +", місяць виробництва " + dateOfManufacture[1] + " , рік виробництва " + dateOfManufacture[2]);
          System.out.println("Тип двигуна " + typeOfEngine);
-         System.out.println("Максимальна швидкість" + maxSpeedOfCar);
+         System.out.println("Максимальна швидкість " + maxSpeedOfCar);
          System.out.println("Час розгону " + accelerationTime);
          System.out.println("Загальна Кількість пасажирів " + numberOfPassanger);
          System.out.println("Поточна кількість пасажирів " + numberOfPassangerNow);
          System.out.println("Поточна швидкість " + currentSpeed);
-         System.out.println("Кількість колес в машині " + numberOfWheels());
-
+         System.out.println("Кількість цілих колес в машині " + numberOfWheels() + ", з них повністю стертих колес - " + numberOfDestroyedWheels());
+         System.out.println("Текущая максимальная скорость с учетом сноса колес " + currentMaxSpeed());
  }
 }
 
--- src/oopPractice/Main.java
+++ src/oopPractice/Main.java
@@ -19,14 +19,14 @@ public class Main {
         System.out.println("Целостность колеса равна " + wheel.getTireState());
         wheel.changeTire();
         System.out.println("Целостность колеса равна " + wheel.getTireState());
-        System.out.println("Введите количество колес в машине");
+      //  System.out.println("Введите количество колес в машине");
      //   int x = sc.nextInt();
-        Car ferrari = new Car(28, 12, 1989);
+        Car ferrari = new Car(28, 12, 1989, "електрический двигатель", 350, 10.5f, 6, 2, 60);
         System.out.println("Сколько колес будет у машини");
         int numberOfTires = sc.nextInt();
         ferrari.otherCarwheel = new CarWheel[numberOfTires];
         for (int i =0; i<ferrari.otherCarwheel.length; i++) {
-            ferrari.otherCarwheel[i] = new CarWheel(0);
+            ferrari.otherCarwheel[i] = new CarWheel(1);
         }
         ferrari.otherCarwheel[0] = new CarWheel(0.7f);
         for (int i =0; i<ferrari.otherCarwheel.length; i++) {
@@ -38,6 +38,9 @@ public class Main {
         }
 
         ferrari.showInfoCar();
+        ferrari.addNewWheels(3);
+        ferrari.showInfoCar();
+
     }
 
 
